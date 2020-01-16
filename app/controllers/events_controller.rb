90d8bfea98da1966624380bf4eca3b7f97de0126
Nairon <saske11@mail.ru>
2020-01-16 17:44:30 +0400

    allow fixes to places and posts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,15 @@ class EventsController < ApplicationController
 	end
 	def show
 		@event = Event.find(params[:id])
-		@places = Place.where(event_id: [@event.subtree_ids]).active #ancestry need to optimized
+		@places = Place.where(event_id: [@event.subtree_ids]).active.page(params[:page])
+		if params[:order]
+			@places.order!(params[:order])
+		else
+			@places.order!('title')
+			#@places.order!('CASE WHEN adwpos IS NULL THEN 1 ELSE 0 END, adwpos')
+		end
+		@custom_paginate_renderer = custom_paginate_renderer
+		render layout: "catalog_place" #ancestry need to optimized
 
 	end
 	def edit
--- app/controllers/tidings_controller.rb
+++ app/controllers/tidings_controller.rb
@@ -15,7 +15,15 @@ class TidingsController < ApplicationController
 	end
 	def show
 		@tiding = Tiding.find(params[:id])
-		@posts = Post.where(tiding_id: [@tiding.subtree_ids]) #ancestry need to optimized
+		@posts = Post.where(tiding_id: [@tiding.subtree_ids]).page(params[:page]) #ancestry need to optimized
+		if params[:order]
+			@posts.order!(params[:order])
+		else
+			@posts.order!('title')
+			#@posts.order!('CASE WHEN adwpos IS NULL THEN 1 ELSE 0 END, adwpos')
+		end
+		@custom_paginate_renderer = custom_paginate_renderer
+		render layout: "catalog_posts"
 
 	end
 	def edit
